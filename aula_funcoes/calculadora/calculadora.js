44fa635087d7270e4731ae911d3ee1765b545131
lucas-dorneles-dev <lucas.dorneles.dev@gmail.com>
2026-08-05 12:10:21 -0300

    calculadora feita em JS, para estudos voltados a factory function

--- aula_funcoes/calculadora/calculadora.js
+++ aula_funcoes/calculadora/calculadora.js
@@ -1,29 +1,29 @@
 function criaCalculadora() {
   return {
     //Atributos  
-    display: document.querySelector('.display'),
+    display: document.querySelector('.display'), // esse eu não entendi muito bem
   //Métodos
-  inicia: function (){
+  inicia: function (){ // o inicia precisa ser chamado para o programa existir, ele faz funcionar os clicks e keys
     this.clickButton();
     this.pessEnter();
   },
   //
-  pessEnter(){
+  pessEnter(){ // para o enter do teclado funcionar na "borda de subida"
     this.display.addEventListener('keyup', k =>{
-      if (k.keyCode == 13){
+      if (k.key === 'Enter'){
         this.relizaConta();
       }
     });
   },
 
-  clearDisplay(){
+  clearDisplay(){ //limpo o display
   this.display.value = "";
 },
-  deleteOne(){
+  deleteOne(){ //aqui eu deleto o ultimo valor inserido na calculadora
       this.display.value = this.display.value.slice(0, -1);
       
     },
-  relizaConta(){
+  relizaConta(){ //essa é para o = e o enter fazer a conta
       let conta = this.display.value;
 
       try {
@@ -38,11 +38,12 @@ function criaCalculadora() {
         return;
       }
     },
-    compraPremim(){
+    compraPremim(){ //essa aqui foi uma brincadeira para testar conceitos mesmo
       alert("Conta apagada, assine para ter a calculadora completa");
       this.clearDisplay();
     },
-  //
+  // essa função precisa ocorrer para os clicks do mouse acontecerem
+    // por exemplo: clicar no C limpa o display
   clickButton(){
     document.addEventListener('click', (e) => {
       const el = e.target;
