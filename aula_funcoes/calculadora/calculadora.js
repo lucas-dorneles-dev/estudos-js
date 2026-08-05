2e786455b3885879ab620b3e5df36b4804103d75
lucas-dorneles-dev <lucas.dorneles.dev@gmail.com>
2026-08-05 00:31:52 -0300

    feat: todas as operações funcionando

--- aula_funcoes/calculadora/calculadora.js
+++ aula_funcoes/calculadora/calculadora.js
@@ -2,23 +2,68 @@ function criaCalculadora() {
   return {
     //Atributos  
     display: document.querySelector('.display'),
-    //Métodos
-    inicia: function (){
+  //Métodos
+  inicia: function (){
     this.clickButton();
+    this.pessEnter();
+  },
+  //
+  pessEnter(){
+    this.display.addEventListener('keyup', k =>{
+      if (k.keyCode == 13){
+        this.relizaConta();
+      }
+    });
+  },
+
+  clearDisplay(){
+  this.display.value = "";
+},
+  deleteOne(){
+      this.display.value = this.display.value.slice(0, -1);
+      
     },
-    
-    clickButton(){
+  relizaConta(){
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+        if (!conta) {
+          alert("Conta invalida");
+          return;
+        }
+        this.display.value = conta;
+      } catch (e) {
+        alert("conta invalida do catch");
+        return;
+      }
+    },
+    compraPremim(){
+      alert("Conta apagada, assine para ter a calculadora completa");
+      this.clearDisplay();
+    },
+  //
+  clickButton(){
     document.addEventListener('click', (e) => {
       const el = e.target;
 
       if(el.classList.contains('btn-num')){
         this.btnToDisplay(el.innerText);
       }
+      if (el.classList.contains('btn-clear')) {
+        this.clearDisplay();
+      }
+      if (el.classList.contains('btn-deleta')) {
+        this.deleteOne();
+      }
+      if (el.classList.contains('btn-equal')) {
+        this.compraPremim();
+      }
     });
-    },
+  },
     btnToDisplay(valor){
-      this.display.value += valor;  
-    }
+  this.display.value += valor;
+}
   };
   }
 
